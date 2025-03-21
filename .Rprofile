d1675edf401b42050e58aa67b18622867984d5d7
nkchrist <Christelle.Ngueda-Kemda@med.uni-jena.de>
2025-03-21 14:19:08 +0100

    functions.R created

--- .Rprofile
+++ .Rprofile
@@ -1,2 +1,3 @@
 source("renv/activate.R")
 source("R/libraries.R")
+source("R/functions.R")
---
+++ R/functions.R
@@ -0,0 +1,13 @@
+#load all function in workspace
+
+OSMI()
+OSMI_island()
+OSMI_on_array_density()
+eliminateMissingsMulti()
+extendedBetaData_prep()
+impute_missing_CpGs()
+perfomance_check()
+perfomance_plot()
+sample_generator()
+use_osmi()
+use_osmi_island()
