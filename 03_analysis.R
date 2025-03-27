a59bd66eef5985bf3abab2edfc6cffcb514ce96d
Julia Palm <julia.palm@med.uni-jena.de>
2025-03-27 15:32:28 +0100

    fix result name

--- 03_analysis.R
+++ 03_analysis.R
@@ -8,13 +8,13 @@ performance_result <- performance_check(nRows = c(400, 1000, 3000, 5000),
 
 #Create Plots for manuscript
 #Plots for Figure 4: Accuracy of missing DNA-methylation value imputation (RMSE)
-plots_figure_4 <- performance_plot(result[[1]])
+plots_figure_4 <- performance_plot(performance_result[[1]])
 
 #Plots for Figure 1 Supplement: Accuracy of missing DNA-methylation value imputation (MAE)
-plots_figure_1_supplement <- performance_plot(result[[2]])
+plots_figure_1_supplement <- performance_plot(performance_result[[2]])
 
 #Plots for Figure 5: Computational time in seconds of missing DNA-methylation value imputations 
-plots_figure_5 <- performance_plot(result[[3]])
+plots_figure_5 <- performance_plot(performance_result[[3]])
 
 #Data for Figure 6
 result_osmi_island <- use_osmi_island(blood_download, ncores = 1)
